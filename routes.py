4b4c36bdaba77396608e7106f7d21c114ef69e60
monterone <slavik.karagezyan19@gmail.com>
2023-12-25 19:34:57 +0400

    crud for assignments

--- routes.py
+++ routes.py
@@ -37,7 +37,8 @@ def get_all_projects():
     session = DBSession()
     try:
         projects = session.query(Project).all()
-        project_list = [{"id": project.id, "name": project.name, "code": project.code, "complexity": project.complexity, "deadline": str(project.deadline)} for project in projects]
+        project_list = [{"id": project.id, "name": project.name, "code": project.code, "complexity": project.complexity,
+                         "deadline": str(project.deadline)} for project in projects]
         return jsonify({"projects": project_list}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -51,7 +52,8 @@ def get_project_by_id(project_id):
     try:
         project = session.query(Project).filter_by(id=project_id).first()
         if project:
-            return jsonify({"project": {"id": project.id, "name": project.name, "code": project.code, "complexity": project.complexity, "deadline": str(project.deadline)}}), 200
+            return jsonify({"project": {"id": project.id, "name": project.name, "code": project.code,
+                                        "complexity": project.complexity, "deadline": str(project.deadline)}}), 200
         else:
             return jsonify({"message": "Project not found"}), 404
     except Exception as e:
@@ -72,7 +74,9 @@ def update_project(project_id):
             project.complexity = data.get("complexity", project.complexity)
             project.deadline = data.get("deadline", project.deadline)
             session.commit()
-            return jsonify({"message": "Project updated successfully", "project": {"id": project.id, "name": project.name, "code": project.code, "complexity": project.complexity, "deadline": str(project.deadline)}}), 200
+            return jsonify({"message": "Project updated successfully",
+                            "project": {"id": project.id, "name": project.name, "code": project.code,
+                                        "complexity": project.complexity, "deadline": str(project.deadline)}}), 200
         else:
             return jsonify({"message": "Project not found"}), 404
     except Exception as e:
@@ -98,6 +102,93 @@ def delete_project(project_id):
         return jsonify({"error": str(e)}), 500
     finally:
         session.close()
+
+
+@app.route('/assignments', methods=['POST'])
+def create_assignment():
+    data = request.get_json()
+    project_id = data.get('project_id')
+    employee_id = data.get('employee_id')
+    role = data.get('role')
+
+    if not all([project_id, employee_id, role]):
+        return jsonify({'error': 'Missing data in the request'}), 400
+
+    with DBSession() as session:
+        project = session.query(Project).get(project_id)
+        employee = session.query(Employee).get(employee_id)
+
+        if not project or not employee:
+            return jsonify({'error': 'Project or employee not found'}), 404
+
+        assignment = Assignment(project=project, employee=employee, role=role)
+
+        try:
+            session.add(assignment)
+            session.commit()
+            return jsonify({'message': 'Assignment created successfully', 'assignment': assignment.serialize()}), 201
+        except Exception as e:
+            session.rollback()
+            return jsonify({'error': str(e)}), 500
+
+
+@app.route('/assignments', methods=['GET'])
+def get_assignments():
+    session = DBSession()
+    assignments = session.query(Assignment).all()
+    return jsonify({'assignments': [assignment.serialize() for assignment in assignments]})
+
+
+@app.route('/assignments/<int:assignment_id>', methods=['GET'])
+def get_assignment(assignment_id):
+    session = DBSession()
+    assignment = session.query(Assignment).get(assignment_id)
+
+    if assignment:
+        return jsonify({'assignment': assignment.serialize()})
+    else:
+        return jsonify({'error': 'Assignment not found'}), 404
+
+
+@app.route('/assignments/<int:assignment_id>', methods=['PUT'])
+def update_assignment(assignment_id):
+    session = DBSession()
+
+    data = request.get_json()
+    new_role = data.get('role')
+
+    assignment = session.query(Assignment).get(assignment_id)
+
+    if not assignment:
+        return jsonify({'error': 'Assignment not found'}), 404
+
+    if new_role:
+        assignment.role = new_role
+
+    try:
+        session.commit()
+        return jsonify({'message': 'Assignment updated successfully', 'assignment': assignment.serialize()})
+    except Exception as e:
+        session.rollback()
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/assignments/<int:assignment_id>', methods=['DELETE'])
+def delete_assignment(assignment_id):
+    session = DBSession()
+
+    assignment = session.query(Assignment).get(assignment_id)
+
+    if not assignment:
+        return jsonify({'error': 'Assignment not found'}), 404
+
+    try:
+        session.delete(assignment)
+        session.commit()
+        return jsonify({'message': 'Assignment deleted successfully'})
+    except Exception as e:
+        session.rollback()
+        return jsonify({'error': str(e)}), 500
 
 
 if __name__ == "__main__":
